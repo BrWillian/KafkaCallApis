508a87077f7742ba1c97e9d5a5b86d0ea6073a14
willian antunes <willianmoreira@vizentec.com.br>
2021-10-20 15:32:20 -0300

    updated methods

--- services/consume.go
+++ services/consume.go
@@ -10,8 +10,8 @@ import (
 
 const (
 	ocr_api           = "http://192.168.250.110:5001/api/ocr"
-	capacete_api      = ""
-	classificador_api = ""
+	capacete_api      = "http://127.0.0.1:5002/api/1.0/capacete"
+	classificador_api = "http://192.168.250.110:5003/api/classificador/veicular"
 )
 
 func ConsumeOcrApi(image string) []byte {
@@ -20,6 +20,45 @@ func ConsumeOcrApi(image string) []byte {
 	jsonValue, _ := json.Marshal(jsonData)
 
 	request, _ := http.NewRequest("POST", ocr_api, bytes.NewBuffer(jsonValue))
+
+	request.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+
+	response, err := client.Do(request)
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	data, _ := ioutil.ReadAll(response.Body)
+
+	return data
+}
+func ConsumeCapaceteApi(image string) []byte {
+	jsonData := map[string]string{"image": image}
+	jsonValue, _ := json.Marshal(jsonData)
+
+	request, _ := http.NewRequest("POST", capacete_api, bytes.NewBuffer(jsonValue))
+
+	request.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+
+	response, err := client.Do(request)
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	data, _ := ioutil.ReadAll(response.Body)
+
+	return data
+}
+
+func ConsumeClassificadorApi(image string) []byte {
+	jsonData := map[string]string{"image": image}
+	jsonValue, _ := json.Marshal(jsonData)
+
+	request, _ := http.NewRequest("POST", classificador_api, bytes.NewBuffer(jsonValue))
 
 	request.Header.Set("Content-Type", "application/json")
 
